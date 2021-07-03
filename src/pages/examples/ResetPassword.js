4350d2a52d756bde2719f55d60633c033c29b9f5
manjunath <manju72nath@gmail.com>
2021-07-03 18:28:04 +0530

    added forgot and reset page

--- src/pages/examples/ResetPassword.js
+++ src/pages/examples/ResetPassword.js
@@ -71,7 +71,7 @@ export default class ResetPassword extends Component{
 
     }
     if(this.state.password === this.state.confirmpassword){
-      axios.post('https://carrombackend.herokuapp.com/admin/reset', customer)
+      axios.post('https://carrombackend.herokuapp.com/admin/reset/:token', customer)
       .then(res => alert("password saved "));
     }
 else
--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ export const Routes = {
     ResetPassword: { path: "/examples/reset/:token" },
     Lock: { path: "/examples/lock" },
     NotFound: { path: "/examples/404" },
-    ServerError: { path: "/examples/500" },
+    ServerError: { path: "/examples/500:token" },
 
     // docs
     DocsOverview: { path: "/documentation/overview" },
